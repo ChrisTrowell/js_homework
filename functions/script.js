551a81bbf7b62dba7059bf4202396237d1be2fd5
Chris Trowell <chris.a.trowell@gmail.com>
2017-05-19 16:52:16 -0400

    Added function examples

---
+++ functions/script.js
@@ -0,0 +1,41 @@
+// Examples from Function Slides
+
+
+// function sayMyName() {
+//         console.log('Hi Claire!');
+//     }
+// sayMyName();
+function sayMyName(name) {
+        console.log('Hi, ' + name);
+    }
+
+ sayMyName('Claire');
+ sayMyName('Testy McTesterFace');
+
+
+function addNumbers(num1, num2) {
+        var result = num1 + num2;
+        console.log(result);
+    }
+
+ 
+ addNumbers(7, 21);
+ addNumbers(3, 10);
+
+
+
+ var number = 10;
+ addNumbers(number, 2);
+ addNumbers(number, 4);
+
+
+
+// *** End Function Examples ***
+
+
+
+
+
+
+
+
